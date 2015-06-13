4cfd5192f96b723022ee83141e1f8ce3330ae092
jipson7 <caleb.phillips@uoit.net>
2015-06-13 19:20:27 -0400

    Add shell script to create LM from text file

---
+++ scripts/buildlm.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+function displayHelp() {
+    echo "Invalid input arguments"
+    echo "usage:"
+    echo "       ./buildlm.sh -i <someinput.txt> -o <someoutput.binlm>"
+    echo ""
+    exit 0
+}
+
+function buildDefaultLm() {
+    wfreq=".wfreq"
+    vocab=".vocab"
+    idngram=".idngram"
+    mkdir -p intermediate-files/$outfilebody/temp
+
+    cat $infile | text2wfreq > intermediate-files/$outfilebody/$outfilebody$wfreq
+
+    cat intermediate-files/$outfilebody/$outfilebody$wfreq | wfreq2vocab > \
+        intermediate-files/$outfilebody/$outfilebody$vocab
+
+    cat $infile | text2idngram -temp intermediate-files/$outfilebody/temp \
+        -vocab intermediate-files/$outfilebody/$outfilebody$vocab > \
+        intermediate-files/$outfilebody/$outfilebody$idngram
+
+    idngram2lm -idngram intermediate-files/$outfilebody/$outfilebody$idngram \
+        -vocab intermediate-files/$outfilebody/$outfilebody$vocab \
+        -binary ./$outfilebody$outfileext
+
+}
+
+
+function checkIfFilesExist(){
+    if [ ! -f "$infile" ]; then
+        echo "Input file does not exist, exiting..."
+        exit 0
+    fi
+    if [ -f "$outfile" ]; then
+        echo "$outfile already exists."
+        while true; do
+            read -p "Do you wish to override it? " yn
+            case $yn in
+                [Yy]* ) rm $outfile; break;;
+                [Nn]* ) echo "exiting..."; exit;;
+                * ) echo "Please enter yes or no";;
+            esac
+        done
+    fi
+
+    buildDefaultLm
+}
+dot="."
+infile=$2
+outfile=$4
+
+if [ -z "$infile" ] || [ -z "$outfile" ]; then
+    displayHelp
+fi
+
+outfilebody=${outfile%.*}
+outfileext=$dot${outfile##*.}
+
+if [ "$1" = "-i" ] && [ "$3" = "-o" ]; then
+    checkIfFilesExist
+else
+    displayHelp
+fi
+
