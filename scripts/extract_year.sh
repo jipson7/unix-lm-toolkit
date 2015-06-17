2be56c49b7e1cc2ac2e55952d99ff2440f4c0d28
jipson7 <caleb.phillips@uoit.net>
2015-06-17 19:25:26 -0400

    Refactored so that python handles output by piping it back to script, making both scripts shorter

--- scripts/extract_year.sh
+++ scripts/extract_year.sh
@@ -3,55 +3,43 @@
 function displayHelp(){
     echo "Invalid Arguments"
     echo "Proper usage is: "
-    echo "      ./extract_year.sh <input-dir> <output-dir> <year>"
+    echo "      ./extract_year.sh <input-dir> <output-file> <year>"
     echo ""
     echo "<input-dir>           : Directory containing zipped google gz files"
-    echo "<output-dir>          : Directory to place processed files in"
+    echo "<output-file>         : File to output google-year ngram to"
     echo "<year>                : year to extract from google ngrams"
     exit 0
 }
 
 function execute(){
-    mkdir -p $output_dir
 
-    finalfile="google-lm-$year"
-
-    if [ -f $finalfile ]; then
-        echo "Cannot merge files. $finalfile already exists"
-    fi
-
-    touch $output_dir/$finalfile
-
+    touch $output_file
 
     for f in $ngram_dir/*.gz; do
         echo "Processing $f ..."
-        STEM=$(basename "${f}" .gz)
-		yearext="-$year"
-		zcat < $f | python ngram_splitter.py $output_dir/$STEM$yearext $year
-
-        echo "Merging $STEM$yearext ..."
-
-        cat $output_dir/$STEM$yearext >> $output_dir/$finalfile
+        zcat < $f | python line_extract.py $year >> $output_file
     done
-
-    
 
 }
 
 ngram_dir=$1
-output_dir=$2
+output_file=$2
 year=$3
 
-if [ -z "$ngram_dir" ] || [ -z "$output_dir" ] || [ -z "$year" ]; then
+if [ -z "$ngram_dir" ] || [ -z "$output_file" ] || [ -z "$year" ]; then
     displayHelp
 fi
 
 if [ ! -d "$ngram_dir" ]; then
-    displayHelp
+    echo "$ngram_dir does not exist. Please ensure you entered the proper path"
 fi
 
-if (( $year > 2008 )) || (( $year < 1500 )); then
-    displayHelp
+if [ -f "$output_file" ]; then
+    echo "$output_file already exists, please remove it before running"
+fi
+
+if [ "$year" -gt 2008 -a "$year" -lt 1500 ]; then
+    echo "Year must be between 1500 and 2008, please check the argument before continuing."
 fi
 
 execute
---
+++ scripts/line_extract.py
@@ -0,0 +1,21 @@
+import sys
+
+def year_in_line(line, year):
+    return (line.split()[-3] == str(year))
+
+def strip_data(line):
+    """Removes the year column and number
+    of books column. Keeps the ngram and the count"""
+    line_items = line.split()
+    clean_data = line_items[:-3] + [line_items[-2]]
+    return (' '.join(clean_data))
+
+def extract(year):
+    for line in sys.stdin:
+        if year_in_line(line, year):
+            print(strip_data(line))
+  
+
+if __name__ == "__main__":
+    year = sys.argv[1] 
+    extract(year)
