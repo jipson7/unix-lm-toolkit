61471be9077f4ac70da21da3a268007e14bd6ff7
jipson7 <caleb.phillips@uoit.net>
2015-06-25 18:18:26 -0400

    Add untested script to build lm from w5gram

---
+++ scripts/buildlm_from_wngram.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+function displayHelp() {
+    echo "Invalid input arguments"
+    echo "usage:"
+    echo "       ./buildlm_from_wngram.sh <someinput.w5gram> <output-dir>"
+    echo ""
+    echo "       It is suggested to use an empty output directory..."
+    echo ""
+    exit 0
+}
+
+function buildDefaultLm() {
+    tempdir="$outdir/temp"
+    mkdir -p $tempdir
+    vocabext=".vocab"
+    idgramext=".idgram"
+    vocabfile=$tempdir/$infileroot$vocabext
+    idgramfile=$tempdir/$infileroot$idgramext
+    binext=".binlm"
+    binfile=$outdir/$infileroot$binext
+    python create_vocab.py $infile $vocabfile
+    wngram2idngram -vocab $vocabfile -temp $tempdir -n 5 < $infile > $idgramfile
+    idngram2lm -idngram $idgramfile -vocab $vocabfile -bin_input -n 5 -binary $binfile
+    echo "All done!"
+
+}
+
+
+function checkIfFilesExist(){
+    if [ ! -f "$infile" ]; then
+        echo "Input file does not exist, exiting..."
+        exit 0
+    fi
+
+    if [ ! -d "$outdir" ]; then
+        echo "Output directory does not exist"
+        while true; do
+            read -p "Do you wish to create it? " yn
+            case $yn in
+                [Yy]* ) mkdir $outdir; break;;
+                [Nn]* ) echo "exiting..."; exit;;
+                * ) echo "Please enter yes or no";;
+            esac
+        done
+    fi
+
+    buildDefaultLm
+}
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    displayHelp
+fi
+
+dot="."
+infile=$1
+outdir=$2
+
+tempfile="${infile##*/}"
+
+infileroot="${tempfile%.*}"
+
+checkIfFilesExist
